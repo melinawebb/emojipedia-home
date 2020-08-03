17e9999564fc17ae46f830b7076f73faa9f3c052
melinawebb <melinawebb@berkeley.edu>
2020-08-02 18:45:05 -0700

    Updates

---
+++ precache-manifest.8dca9fa1c3e68c8007881326d9ac833b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6bb50e5f5f580c5e4266f7fb96f158",
+    "url": "/emojipedia-home/index.html"
+  },
+  {
+    "revision": "702d53259c9c284097b4",
+    "url": "/emojipedia-home/static/js/2.5bb1c6f2.chunk.js"
+  },
+  {
+    "revision": "9652bce05bf9966976db",
+    "url": "/emojipedia-home/static/js/main.4cbe138e.chunk.js"
+  },
+  {
+    "revision": "d6573455e2a3306063d8",
+    "url": "/emojipedia-home/static/js/runtime-main.a6432ad0.js"
+  }
+]);
